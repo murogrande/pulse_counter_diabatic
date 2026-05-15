fix a sign error

pulse_counter_diabatic/counter_diabatic.py
@@ -102,9 +102,9 @@ class CounterDiabaticPulse:
                 break
 
         with torch.no_grad():
-            self.omegas_ising -= a
-            self.mus_ising -= b
-            self.nus_ising -= c
+            self.omegas_ising += a
+            self.mus_ising += b
+            self.nus_ising += c
         r, i, delta, interaction = from_ising_to_rydberg(
             self.omegas_ising,
             self.mus_ising,

tests/test_A_et_b.py
@@ -266,7 +266,7 @@ def test_b_direct_3_qubits():
 def test_2_qubit_sequence():
     n_qubits = 2
 
-    reg = pulser.Register.rectangle(1, n_qubits, prefix="q", spacing=torch.tensor(4))
+    reg = pulser.Register.rectangle(1, n_qubits, prefix="q", spacing=torch.tensor(8))
 
     T = 1000
 
@@ -325,4 +325,64 @@ def test_2_qubit_sequence():
         ]
     )
 
-    assert afm[-1] > 0.9
+    assert afm[-1] > 1 - 1e-8  # for 2 qubits, the counter term is exact
+
+
+def test_3_qubit_sequence():
+    n_qubits = 3
+
+    reg = pulser.Register.rectangle(1, n_qubits, prefix="q", spacing=torch.tensor(8))
+
+    T = 1000
+
+    adiabatic_pulse = pulser.Pulse(
+        pulser.InterpolatedWaveform(
+            T, 2 * torch.sin(torch.arange(0, torch.pi, torch.pi / T))
+        ),
+        pulser.InterpolatedWaveform(
+            T, -2 * torch.cos(torch.arange(0, torch.pi, torch.pi / T))
+        ),
+        0,
+    )
+    seq = pulser.Sequence(reg, pulser.MockDevice)
+    seq.declare_channel("ising_global", "rydberg_global")
+    seq.add(adiabatic_pulse, "ising_global")
+
+    dt = 1
+    config = emu_sv.SVConfig(dt=dt, observables=[emu_sv.Occupation()])
+
+    counter_diabatic_pulse = CounterDiabaticPulse(seq, config)
+    domegas, dmus, dnus = counter_diabatic_pulse.compute_derivatives_numerically()
+
+    time_index_dim = counter_diabatic_pulse.omegas_ising.shape[0]
+    coeffs = torch.zeros(
+        (
+            time_index_dim,
+            3 * n_qubits
+            + 3 * n_qubits * (n_qubits - 1) // 2
+            + 3 * n_qubits * (n_qubits - 1),
+        ),
+        dtype=torch.float64,
+    )
+    for k in range(time_index_dim):
+        M_t = A_direct_mat(
+            counter_diabatic_pulse.n_atoms,
+            counter_diabatic_pulse.omegas_ising[k],
+            counter_diabatic_pulse.mus_ising[k],
+            counter_diabatic_pulse.nus_ising[k],
+            counter_diabatic_pulse.interaction_mat_ising,
+        )
+        b_t = b_direct_vec(n_qubits, domegas[k], dmus[k], dnus[k])
+        coeffs[k] = solve_cd_tikhonov(M_t, b_t)
+    sol = simulate_qutip(
+        counter_diabatic_pulse.omegas_ising,
+        counter_diabatic_pulse.mus_ising,
+        counter_diabatic_pulse.nus_ising,
+        counter_diabatic_pulse.interaction_mat_ising,
+        coeffs,
+        [x + 0.5 for x in range(time_index_dim)],
+    )
+
+    afm = np.array([np.abs(x.full()[5].item()) ** 2 for x in sol.states])
+
+    assert afm[-1] > 0.93  # for 3 qubits, the counter term is not exact

tests/utils.py
@@ -31,19 +31,19 @@ def simulate_qutip(omega, mu, nu, interaction, counter_terms, times):
         op_list.append(
             [
                 single_qubit_gate(n_qubits, i, x),
-                (omega[:, i] - counter_terms[:, 3 * i]).detach().numpy(),
+                (omega[:, i] + counter_terms[:, 3 * i]).detach().numpy(),
             ]
         )
         op_list.append(
             [
                 single_qubit_gate(n_qubits, i, y),
-                (mu[:, i] - counter_terms[:, 3 * i + 1]).detach().numpy(),
+                (mu[:, i] + counter_terms[:, 3 * i + 1]).detach().numpy(),
             ]
         )
         op_list.append(
             [
                 single_qubit_gate(n_qubits, i, z),
-                (nu[:, i] - counter_terms[:, 3 * i + 2]).detach().numpy(),
+                (nu[:, i] + counter_terms[:, 3 * i + 2]).detach().numpy(),
             ]
         )
     for i in range(n_qubits):
@@ -62,62 +62,64 @@ def simulate_qutip(omega, mu, nu, interaction, counter_terms, times):
         op_list.append(
             [
                 two_qubit_gate(n_qubits, c[0], c[1], x, x),
-                -counter_terms[:, n_single + 3 * i].detach().numpy(),
+                +counter_terms[:, n_single + 3 * i].detach().numpy(),
             ]
         )
         op_list.append(
             [
                 two_qubit_gate(n_qubits, c[0], c[1], y, y),
-                -counter_terms[:, n_single + 3 * i + 1].detach().numpy(),
+                +counter_terms[:, n_single + 3 * i + 1].detach().numpy(),
             ]
         )
         op_list.append(
             [
-                two_qubit_gate(n_qubits, c[0], c[1], y, y),
-                -counter_terms[:, n_single + 3 * i + 2].detach().numpy(),
+                two_qubit_gate(n_qubits, c[0], c[1], z, z),
+                +counter_terms[:, n_single + 3 * i + 2].detach().numpy(),
             ]
         )
 
         op_list.append(
             [
                 two_qubit_gate(n_qubits, c[0], c[1], x, y),
-                -counter_terms[:, n_single + n_sym + 3 * i].detach().numpy(),
+                +counter_terms[:, n_single + n_sym + 6 * i].detach().numpy(),
             ]
         )
         op_list.append(
             [
                 two_qubit_gate(n_qubits, c[0], c[1], x, z),
-                -counter_terms[:, n_single + n_sym + 3 * i + 1].detach().numpy(),
+                +counter_terms[:, n_single + n_sym + 6 * i + 1].detach().numpy(),
             ]
         )
         op_list.append(
             [
                 two_qubit_gate(n_qubits, c[0], c[1], y, z),
-                -counter_terms[:, n_single + n_sym + 3 * i + 2].detach().numpy(),
+                +counter_terms[:, n_single + n_sym + 6 * i + 2].detach().numpy(),
             ]
         )
 
         op_list.append(
             [
                 two_qubit_gate(n_qubits, c[0], c[1], y, x),
-                -counter_terms[:, n_single + n_sym + 3 * i + 3].detach().numpy(),
+                +counter_terms[:, n_single + n_sym + 6 * i + 3].detach().numpy(),
             ]
         )
         op_list.append(
             [
                 two_qubit_gate(n_qubits, c[0], c[1], z, x),
-                -counter_terms[:, n_single + n_sym + 3 * i + 4].detach().numpy(),
+                +counter_terms[:, n_single + n_sym + 6 * i + 4].detach().numpy(),
             ]
         )
         op_list.append(
             [
                 two_qubit_gate(n_qubits, c[0], c[1], z, y),
-                -counter_terms[:, n_single + n_sym + 3 * i + 5].detach().numpy(),
+                +counter_terms[:, n_single + n_sym + 6 * i + 5].detach().numpy(),
             ]
         )
 
     h = qutip.QobjEvo(op_list, tlist=np_times)
 
-    return qutip.sesolve(
+    sol = qutip.sesolve(
         h, qutip.tensor([zero] * n_qubits), np.concatenate([[0], np_times, [1]])
     )
+
+    return sol